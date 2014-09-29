0d5fe8eb0973bb45d5c4ca439a79e5916dc511db
James Barrow <james@pigonahill.com>
2014-09-29 22:37:33 +0200

    Initial Commit

---
+++ FrostKit/FrostKit.h
@@ -0,0 +1,19 @@
+//
+//  FrostKit.h
+//  FrostKit
+//
+//  Created by James Barrow on 29/09/2014.
+//  Copyright (c) 2014 Frostlight Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrostKit.
+FOUNDATION_EXPORT double FrostKitVersionNumber;
+
+//! Project version string for FrostKit.
+FOUNDATION_EXPORT const unsigned char FrostKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrostKit/PublicHeader.h>
+
+
