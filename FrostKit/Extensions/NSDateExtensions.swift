3ade90bf0ae20f373798f2c94d6e3ec2f717bfff
James Barrow <baza207@me.com>
2014-10-01 19:12:28 +0200

    Added more items to the NSDateExtensions.

--- FrostKit/Extensions/NSDateExtensions.swift
+++ FrostKit/Extensions/NSDateExtensions.swift
@@ -8,23 +8,17 @@
 
 import Foundation
 
+public enum DateCompareType {
+    case Before
+    case After
+    case EqualTo
+    case BeforeOrEqualTo
+    case AfterOrEqualTo
+}
+
 extension NSDate {
     
-    public class func minuteInSeconds() -> NSTimeInterval {
-        return 60.0
-    }
-    
-    public class func hourInSeconds() -> NSTimeInterval {
-        return minuteInSeconds() * 60.0
-    }
-    
-    public class func dayInSeconds() -> NSTimeInterval {
-        return hourInSeconds() * 24.0
-    }
-    
-    public class func weekInSeconds() -> NSTimeInterval {
-        return dayInSeconds() * 7.0
-    }
+    // MARK: - Date Checks
     
     public var isToday: Bool {
         
@@ -96,4 +90,209 @@ extension NSDate {
         
         return components.day == day && components.month == month && components.year == year
     }
+    
+    public var isWeekday: Bool {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitWeekday, fromDate: self)
+        let range = calendar.maximumRangeOfUnit(NSCalendarUnit.WeekdayCalendarUnit)
+        if components.weekday == range.location || components.weekday == range.length {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    public var isBeginingOfWeek: Bool {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitWeekday, fromDate: self)
+        if components.weekday == 2 { // Begining of week is Monday == 2
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    public var isEndOfWeek: Bool {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitWeekday, fromDate: self)
+        if components.weekday == 1 { // End of week is Sunday == 1
+            return false
+        } else {
+            return false
+        }
+    }
+    
+    public var isBeginingOfMonth: Bool {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitDay, fromDate: self)
+        if components.day == 1 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    public var isEndOfMonth: Bool {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitDay, fromDate: self)
+        let range = calendar.rangeOfUnit(NSCalendarUnit.DayCalendarUnit, inUnit: NSCalendarUnit.MonthCalendarUnit, forDate: self)
+        if components.day == range.length {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // MARK: - Duration
+    
+    public var day: Int {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+            let components = calendar.components(NSCalendarUnit.CalendarUnitDay, fromDate: self)
+            return components.day
+    }
+    
+    public var hour: Int {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+            let components = calendar.components(NSCalendarUnit.CalendarUnitHour, fromDate: self)
+            return components.hour
+    }
+    
+    public var minute: Int {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+            let components = calendar.components(NSCalendarUnit.CalendarUnitMinute, fromDate: self)
+            return components.minute
+    }
+    
+    public var timeInHours: NSTimeInterval {
+        return NSTimeInterval(hour) + (NSTimeInterval(minute) / NSDate.minuteInSeconds())
+    }
+    
+    public class func minuteInSeconds() -> NSTimeInterval {
+        return 60.0
+    }
+    
+    public class func hourInSeconds() -> NSTimeInterval {
+        return minuteInSeconds() * 60.0
+    }
+    
+    public class func dayInSeconds() -> NSTimeInterval {
+        return hourInSeconds() * 24.0
+    }
+    
+    public class func weekInSeconds() -> NSTimeInterval {
+        return dayInSeconds() * 7.0
+    }
+    
+    public class func daysBetweenDates(#fromDate: NSDate, toDate: NSDate) -> Int {
+        
+        var dateTo: NSDate?
+        var dateFrom: NSDate?
+        var duration: NSTimeInterval = 0
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        calendar.rangeOfUnit(NSCalendarUnit.DayCalendarUnit, startDate: &dateFrom, interval: &duration, forDate: toDate)
+        calendar.rangeOfUnit(NSCalendarUnit.DayCalendarUnit, startDate: &dateTo, interval: &duration, forDate: fromDate)
+        
+        let components = calendar.components(NSCalendarUnit.DayCalendarUnit, fromDate: dateFrom!, toDate: dateTo!, options: NSCalendarOptions.WrapComponents)
+        return components.day
+    }
+    
+    public func daysRemainingInWeek() -> Int {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitWeekday, fromDate: self)
+        var weekdayOfDate = components.weekday
+        // To make Monday == 1
+        weekdayOfDate--
+        if weekdayOfDate < 1 {
+            weekdayOfDate = 7 + weekdayOfDate
+        }
+        return (7 - weekdayOfDate) + 1
+    }
+    
+    public func daysInMonth() -> Int {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let range = calendar.rangeOfUnit(NSCalendarUnit.DayCalendarUnit, inUnit: NSCalendarUnit.MonthCalendarUnit, forDate: self)
+        return range.length
+    }
+    
+    public func daysRemainingInMonth() -> Int {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components(NSCalendarUnit.CalendarUnitWeekday, fromDate: self)
+        return (self.daysInMonth() - components.day) + 1
+    }
+    
+    // MARK: - Date Comparison
+    
+    public func compareTo(#date: NSDate, option: DateCompareType = .EqualTo) -> Bool {
+        
+        let compare = self.compare(date)
+        if compare == NSComparisonResult.OrderedAscending {
+            
+            if option == .Before || option == .After {
+                return true
+            }
+        } else if compare == NSComparisonResult.OrderedDescending {
+            
+            if option == .After || option == .AfterOrEqualTo {
+                return true
+            }
+        } else {
+            
+            if option == .EqualTo || option == .BeforeOrEqualTo || option == .AfterOrEqualTo {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    public func isBefore(#date: NSDate) -> Bool {
+        return compareTo(date: date, option: .Before)
+    }
+    
+    public func isAfter(#date: NSDate) -> Bool {
+        return compareTo(date: date, option: .After)
+    }
+    
+    // MARK: -
+    
+    
+    public func stripTime() -> NSDate {
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        let components = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: self)
+        components.timeZone = NSTimeZone.utc()
+        if let date = calendar.dateFromComponents(components) {
+            return date
+        } else {
+            println("Error: Failed to strip time from date \(self)")
+            return self
+        }
+    }
+    
+    public func dateWithDays(days: Int) -> NSDate {
+        
+        let components = NSDateComponents()
+        components.day = days
+        
+        let calendar = NSCalendar.gregorianCalendar()
+        if let date = calendar.dateByAddingComponents(components, toDate: self, options: NSCalendarOptions.WrapComponents) {
+            return date
+        } else {
+            println("Error: Failed to add \(days) days to date \(self)")
+            return self
+        }
+    }
+    
 }
