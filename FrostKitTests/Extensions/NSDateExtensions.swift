62ca3f9676e8192aae6a00262ebd4900576e19e6
James Barrow <baza207@me.com>
2014-10-01 11:35:12 +0200

    Added a sample test for NSDateExtensions isToday.

---
+++ FrostKitTests/Extensions/NSDateExtensions.swift
@@ -0,0 +1,31 @@
+//
+//  NSDateExtensions.swift
+//  FrostKit
+//
+//  Created by James Barrow on 01/10/2014.
+//  Copyright (c) 2014 Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import FrostKit
+
+class NSDateExtensions: XCTestCase {
+   
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIsToday() {
+        
+        let date = NSDate.date()
+        XCTAssert(date.isToday, "Pass")
+    }
+    
+}
