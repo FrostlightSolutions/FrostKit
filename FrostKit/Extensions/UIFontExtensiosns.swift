bbb6a7298125f570f427a759b69a5112d6c6379e
James Barrow <baza207@me.com>
2014-10-02 01:36:35 +0200

    Added FontAwesome class, font and test to allow printing of the parsed .less file into let constants.

---
+++ FrostKit/Extensions/UIFontExtensiosns.swift
@@ -0,0 +1,17 @@
+//
+//  UIFontExtensiosns.swift
+//  FrostKit
+//
+//  Created by James Barrow on 02/10/2014.
+//  Copyright (c) 2014 Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    public class func fontAwesome(#size: CGFloat) -> UIFont {
+        return UIFont(name: "FontAwesome", size: size)
+    }
+    
+}
---
+++ FrostKitTests/Fonts/FontTests.swift
@@ -0,0 +1,29 @@
+//
+//  FontTests.swift
+//  FrostKit
+//
+//  Created by James Barrow on 02/10/2014.
+//  Copyright (c) 2014 Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import FrostKit
+
+class FontTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+//    func testBuildFontAwesomeConstants() {
+//        XCTAssert(FontAwesome.buildFontAwesomeConstants(), "Pass")
+//    }
+    
+}
