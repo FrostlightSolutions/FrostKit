7af78e36b42e444376ed73e0fc698aeb9162d26f
Niels Lemmens <draso.odin@gmail.com>
2014-10-01 11:57:26 +0200

    Simplify NSDateExtensions for readability and simplicity

--- FrostKit/Extensions/NSDateExtensions.swift
+++ FrostKit/Extensions/NSDateExtensions.swift
@@ -10,74 +10,23 @@ import Foundation
 
 extension NSDate {
     
-    public var isToday: Bool {
-        
+    private func dateIsEqual(date: NSDate) -> Bool {
         let calendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)
-        let today = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: NSDate.date())
+        let dateComponents = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: date)
         let components = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: self)
         
-        return components.day == today.day && components.month == today.month && components.year == today.year
+        return components.day == dateComponents.day && components.month == dateComponents.month && components.year == dateComponents.year
+    }
+    
+    public var isToday: Bool {
+        return self.dateIsEqual(NSDate.date())
     }
     
     public var isYesterday: Bool {
-        
-        let calendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)
-        let today = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: NSDate.date())
-        let components = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: self)
-        
-        var day = today.day - 1
-        var month = today.month
-        var year = today.year
-        
-        if day < 1 {
-            
-            month -= 1
-            if month < 0 {
-                
-                month = 12
-                year -= 1
-            }
-            
-            let lastMonthsComponents = NSDateComponents()
-            lastMonthsComponents.month = month
-            lastMonthsComponents.year = year
-            
-            if let lastMonthsDate = calendar.dateFromComponents(lastMonthsComponents) {
-                
-                let maxDaysRange = calendar.rangeOfUnit(.CalendarUnitDay, inUnit: .CalendarUnitMonth, forDate: lastMonthsDate)
-                day = maxDaysRange.length
-            } else {
-                return false
-            }
-        }
-        
-        return components.day == day && components.month == month && components.year == year
+        return self.dateIsEqual(NSDate(timeIntervalSinceNow: -86400)) // -24 hours
     }
     
     public var isTomorrow: Bool {
-        
-        let calendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)
-        let today = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: NSDate.date())
-        let components = calendar.components((NSCalendarUnit.CalendarUnitDay | NSCalendarUnit.CalendarUnitMonth | NSCalendarUnit.CalendarUnitYear), fromDate: self)
-        
-        var day = today.day + 1
-        var month = today.month
-        var year = today.year
-        
-        let maxDaysRange = calendar.rangeOfUnit(.CalendarUnitDay, inUnit: .CalendarUnitMonth, forDate: self)
-        
-        if day > maxDaysRange.length {
-            
-            day = 1
-            month++
-            
-            if month > 12 {
-                
-                month = 1
-                year++
-            }
-        }
-        
-        return components.day == day && components.month == month && components.year == year
+        return self.dateIsEqual(NSDate(timeIntervalSinceNow: 86400)) // 24 hours
     }
 }
