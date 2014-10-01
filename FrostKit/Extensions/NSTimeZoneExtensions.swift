6e845ed35a0a0cf76f6a206dd936eb901ed8c35e
James Barrow <baza207@me.com>
2014-10-01 18:56:10 +0200

    Added NSTimeZoneExtensions.

---
+++ FrostKit/Extensions/NSTimeZoneExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  NSTimeZoneExtensions.swift
+//  FrostKit
+//
+//  Created by James Barrow on 01/10/2014.
+//  Copyright (c) 2014 Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+
+extension NSTimeZone {
+    
+    public class func utc() -> NSTimeZone {
+        return NSTimeZone(name: "UTC")
+    }
+    
+}
