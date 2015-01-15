f088083428ab21ed519d16aed14e968591acf706
James Barrow <baza207@me.com>
2015-01-15 01:58:46 +0100

    Added documentation for ContentManager class and updated build documentation.

--- FrostKit/Storage/ContentManager.swift
+++ FrostKit/Storage/ContentManager.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
+///
+/// The Content Manager is a class that checks all content saved into local storage. It tracks when they were added/accessed from LocalStorage or ImageCache.
+/// If an item has not been accessed in 2 weeks (by default) they will be automatically deleted on launch.
+/// 
+/// To activate this class, call `checkContentMetadata()` in `-application:willFinishLaunchingWithOptions:` to check all the managed files.
+///
 public class ContentManager: NSObject {
     
+    // TODO: contentMetadata needs to be saved in a User.data object (once it's been added to the framework).
+    // A dictioary holding the metadata for all managed objects, where the key is an absolute path and the value is the date.
     private let contentMetadata = NSMutableDictionary()
     
     private class func maxSavedTimeInSeconds() -> NSTimeInterval {
@@ -18,6 +26,11 @@ public class ContentManager: NSObject {
     
     // MARK: - Singleton
     
+    /**
+        Returns the shared content manager object.
+    
+        :returns: The shared content manager object.
+    */
     public class var shared: ContentManager {
         struct Singleton {
             static let instance : ContentManager = ContentManager()
@@ -27,26 +40,9 @@ public class ContentManager: NSObject {
     
     // MARK: - Content Management Methods
     
-    public class func saveContentMetadata(#absoluteURL: NSURL) {
-        if let absolutePath = absoluteURL.path {
-            saveContentMetadata(absolutePath: absolutePath)
-        }
-    }
-    
-    public class func saveContentMetadata(#absolutePath: String) {
-        shared.contentMetadata.setObject(NSDate().copy(), forKey: absolutePath)
-    }
-    
-    public class func removeContentMetadata(#absoluteURL: NSURL) {
-        if let absolutePath = absoluteURL.path {
-            removeContentMetadata(absolutePath: absolutePath)
-        }
-    }
-    
-    public class func removeContentMetadata(#absolutePath: String) {
-        shared.contentMetadata.removeObjectForKey(absolutePath)
-    }
-    
+    /**
+    Checks though all of the managed content metadata. If an item has not been accessed for more than 2 weeks then it is removed from the local storage.
+    */
     public class func checkContentMetadata() {
         
         if shared.contentMetadata.count > 0 {
@@ -81,6 +77,46 @@ public class ContentManager: NSObject {
                 println("Check of \(shared.contentMetadata.count) content metadata items complete in \(finish-start) seconds. Removed \(metadataToRemove.count) items.")
             #endif
         }
+    }
+    
+    /**
+        Creates or updates metadata for an object at an absolute URL.
+    
+        :param: absoluteURL    The absolute URL of the item.
+    */
+    public class func saveContentMetadata(#absoluteURL: NSURL) {
+        if let absolutePath = absoluteURL.path {
+            saveContentMetadata(absolutePath: absolutePath)
+        }
+    }
+    
+    /**
+        Creates or updates metadata for an object at an absolute path.
+        
+        :param: absolutePath    The absolute path of the item.
+    */
+    public class func saveContentMetadata(#absolutePath: String) {
+        shared.contentMetadata.setObject(NSDate().copy(), forKey: absolutePath)
+    }
+    
+    /**
+        Removed metadata for an object at an absolute URL.
+        
+        :param: absoluteURL    The absolute URL of the item.
+    */
+    public class func removeContentMetadata(#absoluteURL: NSURL) {
+        if let absolutePath = absoluteURL.path {
+            removeContentMetadata(absolutePath: absolutePath)
+        }
+    }
+    
+    /**
+        Removed metadata for an object at an absolute path.
+        
+        :param: absolutePath    The absolute path of the item.
+    */
+    public class func removeContentMetadata(#absolutePath: String) {
+        shared.contentMetadata.removeObjectForKey(absolutePath)
     }
     
 }
