1c085c6f40f06ef71cfdf4dc5b582e8ce0e4249e
James Barrow <baza207@me.com>
2015-01-19 14:22:56 +0100

    Add login function to ServiceClient and tests.

---
+++ FrostKitTests/Service/ServiceClientTests.swift
@@ -0,0 +1,48 @@
+//
+//  FrostKitTests.swift
+//  FrostKitTests
+//
+//  Created by James Barrow on 29/09/2014.
+//  Copyright (c) 2014 Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import FrostKit
+
+let expectationTimeout: NSTimeInterval = 60
+
+class ServiceClientTests: XCTestCase {
+    
+    let username = "odin"
+    let password = "odin"
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testLogin() {
+        
+        let expectation = expectationWithDescription("Test Login")
+        
+        ServiceClient.login(username, password: password) { (error) -> () in
+            if let anError = error {
+                println("Login Error: \(anError.localizedDescription)")
+                XCTAssert(false, "Fail")
+            } else {
+                println("Logged In")
+                XCTAssert(true, "Pass")
+            }
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(expectationTimeout, handler: { (completionHandler) -> Void in })
+    }
+    
+}
