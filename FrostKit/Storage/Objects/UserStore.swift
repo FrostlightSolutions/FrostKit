996908d99219c43fe7d76fdaeb71922738607041
James Barrow <baza207@me.com>
2015-01-21 18:06:31 +0100

    Added documentation to UserStore.

--- FrostKit/Storage/Objects/UserStore.swift
+++ FrostKit/Storage/Objects/UserStore.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+/// 
+/// This object contains all to shared data corisponding to the currently logged in user.
+///
+/// This object should be deleted or cleared on a user logging out.
+///
 public class UserStore: NSObject, NSCoding {
 
     private var oAuthToken: OAuthToken?
@@ -49,10 +54,20 @@ public class UserStore: NSObject, NSCoding {
     
     // MARK: - Save / Load Methods
     
+    /**
+    Saves the current UserStore shared instance.
+    
+    :returns: `true` if the save completed successfully, `false` if it failed.
+    */
     class func saveUser() -> Bool {
         return LocalStorage.saveUserData(UserStore.shared)
     }
     
+    /**
+    Loads the UserStore object from local storage if available. If there is no previously saved item, it creates a new one.
+    
+    :returns: A loaded or newly created UserStore object.
+    */
     internal class func loadUser() -> UserStore {
         if let userStore = LocalStorage.loadUserData() as? UserStore {
             return userStore
