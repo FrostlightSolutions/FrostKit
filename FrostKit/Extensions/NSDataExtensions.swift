bb286899bb4905c2a229860b08462f20c174beeb
James Barrow <baza207@me.com>
2015-01-21 12:54:48 +0100

    Added a file data formatter to NSDataExtentions.

--- FrostKit/Extensions/NSDataExtensions.swift
+++ FrostKit/Extensions/NSDataExtensions.swift
@@ -27,4 +27,41 @@ extension NSData {
         return string
     }
     
+    /**
+    Creates a formatted string from the `size` passed in in bytes.
+    
+    :param: size The size of the item in bytes.
+    
+    :returns: A formatted string of the size passed in. E.g. 1024 bytes returns 1MB.
+    */
+    public class func sizeFormattedString(size: Int) -> String {
+        
+        let sUnits = ["\0", "K", "M", "G", "T", "P", "E", "Z", "Y"]
+        let sMaxUnits = sUnits.count - 1
+        
+        let multiplier = 1024.0
+        var exponent = 0
+        
+        var bytes = Double(size)
+        
+        while bytes >= multiplier && exponent < sMaxUnits {
+            bytes /= multiplier
+            exponent++
+        }
+        
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.maximumFractionDigits = 2
+        
+        if let stringSize = numberFormatter.stringFromNumber(NSNumber(double: bytes)) {
+            return NSString(format: "%@ %@B", stringSize, sUnits[exponent])
+        } else {
+            return "Unknown"
+        }
+    }
+    
+    /// Created a formatted string from the objects length value
+    public var lengthFormattedString: String {
+        return NSData.sizeFormattedString(self.length)
+    }
+    
 }
