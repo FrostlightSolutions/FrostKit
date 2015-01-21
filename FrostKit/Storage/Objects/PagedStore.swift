4999c390a93c43dbabd95175c7e28a3de05681ed
James Barrow <baza207@me.com>
2015-01-21 18:00:08 +0100

    Added documentation to PagedStore and made a few code amendments.

--- FrostKit/Storage/Objects/PagedStore.swift
+++ FrostKit/Storage/Objects/PagedStore.swift
@@ -8,31 +8,46 @@
 
 import UIKit
 
+
 protocol PagedArrayDelegate {
     func pagedArray(pagedArray: PagedStore, willAccessIndex: Int, returnObject: AnyObject)
 }
 
+///
+/// Paged store allows storing of data in a paged form. It allows pre populating an array (with NSNull objects) in the store with the total number of object until they are called upon to be updated with the real data.
+///
+/// This class allows for a correct representation of how large a table or collection view is when used like an array in a data source for these UI items. It also allows the ability to only load items when they are needed and with the delegate method cancel or lower priority fast scroll past pages.
+///
+/// For more information on how FUS passes paginated data to clients, check out the Wiki page at: https://github.com/FrostlightSolutions/fus-server/wiki/Pagination-API
+///
 class PagedStore: NSObject, NSCoding, NSCopying {
     
     private lazy var _count = 0
+    /// Returns the total count of object stored.
     var count: Int {
         return objects.count
     }
     private lazy var _objectsPerPage = 0
+    /// Returns the total number of objects per page. All pages will have the same number of objects, apart from the last which might have less.
     var objectsPerPage: Int {
         return _objectsPerPage
     }
+    /// THe number of pages in the store (not loaded, in total).
     var numberOfPages: Int {
         return Int(ceil(Double(_count) / Double(_objectsPerPage)))
     }
+    /// The delegate of the store.
     var delegate: PagedArrayDelegate?
     private lazy var objects = NSArray()
+    /// Thefirst object in the store.
     var firstObject: AnyObject? {
         return objects.firstObject
     }
+    /// The last object in the store.
     var lastObject: AnyObject? {
         return objects.lastObject
     }
+    /// A string that represents the contents of the stores array, formatted as a property list.
     override var description: String {
         return objects.description
     }
@@ -41,14 +56,25 @@ class PagedStore: NSObject, NSCoding, NSCopying {
         super.init()
     }
     
+    /**
+    Initializes a store from anouther store.
+    
+    :param: store The store object to base the new one from.
+    */
     convenience init(store: PagedStore) {
         self.init()
         
         _count = store._count
         _objectsPerPage = store._objectsPerPage
-        objects = store.objects
+        objects = store.objects.copy() as NSArray
     }
     
+    /**
+    Initializes a store object from the total count and the number of objects per page.
+    
+    :param: totalCount     The total count of the store.
+    :param: objectsPerPage The total objects per page. This should be the same for all pages (though it is excepted the last page may not furfil this value).
+    */
     convenience init(totalCount: Int, objectsPerPage: Int) {
         self.init()
         
@@ -62,7 +88,13 @@ class PagedStore: NSObject, NSCoding, NSCopying {
         self.objects = objects
     }
     
-    convenience init(json: NSDictionary, objectsPerPage: Int, page: Int) {
+    /**
+    Initializes a store object from a JSON dictionary returned from FUS. It is assumed that the values returned in the dictionary will always be from page 1 (the first page).
+    
+    :param: json           The JSON dictionary returned from FUS.
+    :param: objectsPerPage The total objects per page. This should be the same for all pages (though it is excepted the last page may not furfil this value).
+    */
+    convenience init(json: NSDictionary, objectsPerPage: Int) {
         var totalCount = 0
         if let count = json["count"] as? Int {
             totalCount = count
@@ -73,9 +105,14 @@ class PagedStore: NSObject, NSCoding, NSCopying {
         }
         
         self.init(totalCount: totalCount, objectsPerPage: objectsPerPage)
-        setObjects(objects, page: page)
+        setObjects(objects, page: 1)
     }
     
+    /**
+    Initializes a store object for a non-paged array of objects returned from FUS. This creates a normal paged store but takes the whole array of objects as page 1 (the first page).
+    
+    :param: nonPagedObjects An array of objects to store.
+    */
     convenience init(nonPagedObjects: NSArray) {
         
         self.init(totalCount: nonPagedObjects.count, objectsPerPage: nonPagedObjects.count)
@@ -109,6 +146,13 @@ class PagedStore: NSObject, NSCoding, NSCopying {
     
     // MARK: - Helper Methods
     
+    /**
+    Sets object into the store for a current page. This will either replace palceholder objects with data or update previous stored objects with the new values. If `totalCount` is returned and if it is different from the previous number then the store will add or remove the relevaent placeholders or objects in the store respectively.
+    
+    :param: newObjects The new objects to add or update into the store.
+    :param: page       The page the objects have come from in FUS.
+    :param: totalCount The updated total objects count.
+    */
     func setObjects(newObjects: NSArray, page: Int, totalCount: Int? = nil) {
         
         let objects = self.objects.mutableCopy() as NSMutableArray
@@ -133,6 +177,13 @@ class PagedStore: NSObject, NSCoding, NSCopying {
         self.objects = objects
     }
     
+    /**
+    Returns the object located at the specified index in the store.
+    
+    :param: index An index within the bounds of the store.
+    
+    :returns: The object located at index.
+    */
     func objectAtIndex(index: Int) -> AnyObject {
         let object: AnyObject = objects[index]
         if let delegate = self.delegate {
@@ -145,14 +196,35 @@ class PagedStore: NSObject, NSCoding, NSCopying {
         return objectAtIndex(idx)
     }
     
+    /**
+    Returns the page number of the specified index in the store.
+    
+    :param: index An index with the bounds of the store.
+    
+    :returns: The page number the index is located in.
+    */
     func pageForIndex(index: Int) -> Int {
         return index / objectsPerPage
     }
     
+    /**
+    Returns the lowest index whose corresponding store value is equal to a given object.
+    
+    :param: anObject The object to find in the store.
+    
+    :returns: The lowest index whose corresponding store value is equal to anObject. If none of the objects in the store is equal to anObject, returns NSNotFound.
+    */
     func indexOfObject(anObject: AnyObject) -> Int {
         return objects.indexOfObject(anObject)
     }
     
+    /**
+    Returns the index set of the objects in the page requested.
+    
+    :param: page The page of the residing index sets.
+    
+    :returns: An index set of the indexes on the given page.
+    */
     func indexSetForPage(page: Int) -> NSIndexSet {
         
         var rangeLength = objectsPerPage
@@ -162,9 +234,20 @@ class PagedStore: NSObject, NSCoding, NSCopying {
         return NSIndexSet(indexesInRange: NSMakeRange((page - 1) * objectsPerPage, rangeLength))
     }
     
+    /**
+    Returns the first page number whose corresponding store value is equal to a given object.
+    
+    :param: anObject The object to find in the store.
+    
+    :returns: The lowest page number whose corresponding store value is equal to anObject. If none of the objects in the store is equal to anObject, returns NSNotFound.
+    */
     func pageForObject(anObject: AnyObject) -> Int {
         let index = indexOfObject(anObject)
-        return Int(floor(Double(index) / Double(objectsPerPage)))
+        if index == NSNotFound {
+            return index
+        } else {
+            return Int(floor(Double(index) / Double(objectsPerPage)))
+        }
     }
     
 }
