ca95e984a8a0555d2e186220932a852896d3ce75
James Barrow <baza207@me.com>
2015-01-27 13:04:14 +0100

    Added an extension for UICollectionViewController to handle refreshControl the same way as a UITableViewController does.

---
+++ FrostKit/Extensions/UICollectionViewControllerExtensions.swift
@@ -0,0 +1,33 @@
+//
+//  UICollectionViewControllerExtensions.swift
+//  FrostKit
+//
+//  Created by James Barrow on 27/01/2015.
+//  Copyright (c) 2015 Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+
+///
+/// Extention functions for UICollectionViewController
+///
+extension UICollectionViewController {
+    /// Allows easy access to a collection view controller's refrsh control the same way as in a table view controller.
+    var refreshControl: UIRefreshControl? {
+        get {
+            return collectionView?.viewWithTag(140412014669856) as? UIRefreshControl
+        }
+        set {
+            if let oldRefreshControl = collectionView?.viewWithTag(140412014669856) as? UIRefreshControl {
+                oldRefreshControl.removeFromSuperview()
+                collectionView?.alwaysBounceVertical = false
+            }
+            
+            if let refreshControl = newValue {
+                refreshControl.tag = 140412014669856
+                collectionView?.addSubview(refreshControl)
+                collectionView?.alwaysBounceVertical = true
+            }
+        }
+    }
+}
