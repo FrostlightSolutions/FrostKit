5063cd268900ff26c72f654511edb373cf66a2bc
James Barrow <baza207@me.com>
2015-02-06 14:16:58 +0100

    Added a new target that builds the included icon fonts constant in a single Swift file.
    This is called from a custom shell script from the FontConstantsBuild target. The script gets a list of less files provided and runs a python script which converts the constants into public Swift constants in a single file.

---
+++ FrostKit/Fonts/ParseFontLessConstants.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+'''
+  ParseFontLessConstants.py
+  FrostKit
+  
+  Created by James Barrow on 06/02/2015.
+  Copyright (c) 2015 Frostlight Solutions. All rights reserved.
+  
+  This script takes in a list of less file names that refer to files in the LessConstantsFiles
+  directory. This script then loops though thouse files and converts them into a single Swift
+  file that contains all the public constants for every icon font included.
+  
+  This script is run from the FontConstantsBuilder target.
+'''
+
+import datetime, sys
+
+def todaysFormattedDate():
+  today = datetime.datetime.now()
+  return today.strftime('%d/%m/%Y %H:%M:%S')
+
+def parseFontConsatnts(inputPath, outputPath):
+  name = inputPath.split('.')[0]
+  contents = ''
+  contents+= '\n/*\n------------------------------\n'
+  contents+= name
+  contents+= '\n------------------------------\n*/\n\n'
+
+  openObject = open('LessConstantsFiles/' + inputPath)
+  openFile = openObject.read()
+
+  for line in openFile.splitlines():
+    if 'var' in line:
+      line = line.replace('@', '')
+      line = line.replace('-var', '')
+      line = line.replace('-', '_')
+      line = line.replace(' ', '')
+      line = line.replace('\"', '')
+      line = line.replace('\\', '')
+      line = line.replace(';', '')
+
+      components = line.split(':')
+      swiftLine = 'public let ' + components[0] +' = \"\\u{' + components[1] + '}\"\n'
+      
+      contents += swiftLine
+
+  return contents
+
+def parseFonts(fonts):
+  outputPath = 'FontConstants.swift'
+  contents = """//
+//  FontConstants.swift
+//  FrostKit
+//
+//  Created by James Barrow on 06/02/2015.
+//  Copyright (c) 2015 Frostlight Solutions. All rights reserved.
+//  Last updated on %s.
+//
+""" %(todaysFormattedDate())
+  
+  for font in fonts:
+    contents += parseFontConsatnts(font, outputPath)
+
+  writeObject = open(outputPath, 'wb')
+  writeObject.write(contents)
+
+def main():
+  fileNames = sys.argv[1].split(' ')
+  parseFonts(fileNames)
+
+if __name__ == "__main__":
+  main()
