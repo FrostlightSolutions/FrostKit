b2017299abc16976bb234691ef89131163b6d218
James Barrow <baza207@me.com>
2015-02-13 00:05:33 +0100

    Added an InitialsImageView that works out the initials for a name passed in and shows the initials unless an image is set.

---
+++ FrostKit/Views/InitialsImageView.swift
@@ -0,0 +1,61 @@
+//
+//  InitialsImageView.swift
+//  FrostKit
+//
+//  Created by James Barrow on 12/02/2015.
+//  Copyright (c) 2015 James Barrow - Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+
+public class InitialsImageView: ImageView {
+    
+    @IBInspectable public var name: String? = "" {
+        didSet {
+            var initials = ""
+            if let aName = name {
+                for word in aName.componentsSeparatedByString(" ") {
+                    initials += ((word as NSString).substringWithRange(NSMakeRange(0, 1)) as String).uppercaseString
+                }
+            }
+            self.initials = initials
+        }
+    }
+    private var initials: String = "" {
+        didSet {
+            layer.borderColor = tintColor?.CGColor
+            initialsLabel.textColor = tintColor
+            initialsLabel?.text = initials
+        }
+    }
+    @IBInspectable public var borderWidth: CGFloat = 1 {
+        didSet {
+            if image == nil {
+                layer.borderWidth = borderWidth
+            }
+        }
+    }
+    override public var image: UIImage? {
+        didSet {
+            if image == nil {
+                initialsLabel.hidden = false
+                layer.borderWidth = borderWidth
+            } else {
+                initialsLabel.hidden = true
+                layer.borderWidth = 0
+            }
+        }
+    }
+    override public var tintColor: UIColor! {
+        didSet {
+            layer.borderColor = tintColor?.CGColor
+            initialsLabel.textColor = tintColor
+        }
+    }
+    @IBOutlet public  weak var initialsLabel: UILabel! {
+        didSet {
+            initialsLabel.backgroundColor = UIColor.clearColor()
+        }
+    }
+    
+}
