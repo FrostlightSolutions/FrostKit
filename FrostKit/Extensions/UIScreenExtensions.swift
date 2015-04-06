509d80da38b8bdf9aa738191ed1324daf9033e43
James Barrow <baza207@me.com>
2015-04-06 19:22:01 +0200

    Added an extension for UIScreen adding a halfPoint variable relative to the screen scale.

---
+++ FrostKit/Extensions/UIScreenExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIScreenExtensions.swift
+//  FrostKit
+//
+//  Created by James Barrow on 06/04/2015.
+//  Copyright (c) 2015 James Barrow - Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+
+extension UIScreen {
+    
+    /// Returns a half point depnding on the scale of the screen size. If the scale is 2.0 or above then `0.5` is returned. Otherwise `1` is returned.
+    public var halfPoint: CGFloat {
+        if scale > 1 {
+            return 0.5
+        }
+        return 1
+    }
+    
+}
