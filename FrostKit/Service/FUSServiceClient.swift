66af673d8fbbb1e86e0e250324166e5d4c1dcb31
James Barrow <baza207@me.com>
2015-07-26 23:59:43 +0200

    Fixed issue with the Alamofire.Request extension adding a responseImage function and image response serialised after Alamofire changes.

--- FrostKit/Service/FUSServiceClient.swift
+++ FrostKit/Service/FUSServiceClient.swift
@@ -480,9 +480,9 @@ public class FUSServiceClient: NSObject {
 }
 
 extension Alamofire.Request {
-    class func imageResponseSerializer() -> Serializer {
-        return { request, response, data in
-            if data == nil {
+    class func imageResponseSerializer() -> GenericResponseSerializer<UIImage> {
+        return GenericResponseSerializer { request, response, data in
+            if data == nil || data?.length == 0 {
                 return (nil, nil)
             }
             
@@ -491,8 +491,10 @@ extension Alamofire.Request {
     }
     
     func responseImage(completionHandler: (NSURLRequest, NSHTTPURLResponse?, UIImage?, NSError?) -> Void) -> Self {
-        return response(serializer: Request.imageResponseSerializer(), completionHandler: { (request, response, image, error) in
-            completionHandler(request, response, image as? UIImage, error)
-        })
+        
+        return response(
+            responseSerializer: Request.imageResponseSerializer(),
+            completionHandler: completionHandler
+        )
     }
 }
