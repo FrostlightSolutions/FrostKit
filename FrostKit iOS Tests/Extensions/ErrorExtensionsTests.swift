a6e60a5285f911873b1d85270999675f04cfa201
James Barrow <baza207@me.com>
2016-02-26 17:06:28 +0100

    Added a test for error extensions.

---
+++ FrostKit iOS Tests/Extensions/ErrorExtensionsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ErrorExtensionsTests.swift
+//  FrostKit
+//
+//  Created by James Barrow on 26/02/2016.
+//  Copyright © 2016 James Barrow - Frostlight Solutions. All rights reserved.
+//
+
+import XCTest
+import FrostKit
+
+class ErrorExtensionsTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testErrorWithMessage() {
+        
+        measureBlock {
+            
+            let message = "This is a test error!"
+            let error = NSError.errorWithMessage(message)
+            XCTAssert(error.localizedDescription == message)
+        }
+    }
+
+}
