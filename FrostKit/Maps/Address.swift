5081a9268606dfca3c91f71a12dae55747afa8f6
James Barrow <baza207@me.com>
2016-04-13 18:56:55 +0200

    Split off Address and Annotation objects into there own files.

---
+++ FrostKit/Maps/Address.swift
@@ -0,0 +1,125 @@
+//
+//  Address.swift
+//  FrostKit
+//
+//  Created by James Barrow on 13/04/2016.
+//  Copyright © 2016 James Barrow - Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+///
+/// An object that contains details on a address to the plot on the map view, along with other data such as name and addressString.
+///
+public class Address: NSObject {
+    
+    /// The ID of the addres object.
+    public var objectID: String?
+    /// The coordinate of the address.
+    public var coordinate = CLLocationCoordinate2DMake(0.0, 0.0)
+    /// The latitude of the address.
+    public var latitude: CLLocationDegrees {
+        return coordinate.latitude
+    }
+    /// The longitude of the address.
+    public var longitude: CLLocationDegrees {
+        return coordinate.longitude
+    }
+    /// If the address is valid then `true`, otherwise return `false`.
+    public var isValid: Bool {
+        if latitude == 0 && longitude == 0 {
+            return false
+        } else {
+            return true
+        }
+    }
+    /// The name of the address object.
+    public var name = ""
+    /// The address string of the object.
+    public var addressString = ""
+    /// Returns a string that represents the contents of the receiving class.
+    override public var description: String {
+        return "<Latitude: \(latitude) Longitude: \(longitude) Address: \(addressString)>"
+    }
+    override public var hash: Int {
+        return Int(latitude) ^ Int(longitude)
+    }
+    
+    public override init() {
+        super.init()
+    }
+    
+    /**
+     A convenience initialiser for creating an address object from a dictionary returned from a FUS based system.
+     
+     - parameter dictionary: The dictionary to parse the information from.
+     */
+    public convenience init(dictionary: NSDictionary) {
+        self.init()
+        
+        objectID = dictionary["id"] as? String
+        
+        if let latitude = dictionary["latitude"] as? Double {
+            coordinate.latitude = latitude
+        } else if let latitude = dictionary["latitude"] as? Float {
+            coordinate.latitude = Double(latitude)
+        } else if let latitude = dictionary["latitude"] as? Int {
+            coordinate.latitude = Double(latitude)
+        }
+        
+        if let longitude = dictionary["longitude"] as? Double {
+            coordinate.longitude = longitude
+        } else if let longitude = dictionary["longitude"] as? Float {
+            coordinate.latitude = Double(longitude)
+        } else if let longitude = dictionary["longitude"] as? Int {
+            coordinate.latitude = Double(longitude)
+        }
+        
+        if let name = dictionary["name"] as? String {
+            self.name = name
+        }
+        
+        if let addressString = dictionary["addressString"] as? String {
+            self.addressString = addressString
+        }
+    }
+    
+    /**
+     A helper method for creating an array of address objects from an array of dictionaryies.
+     
+     - parameter array: An array of dictionaries returned by a FUS like system.
+     
+     - returns: An array of address objects.
+     */
+    public class func addressesFromArrayOfDictionaries(array: [NSDictionary]) -> [Address] {
+        var adresses = [Address]()
+        for dictionary in array {
+            adresses.append(Address(dictionary: dictionary))
+        }
+        return adresses
+    }
+    
+    // MARK: - Comparison Methods
+    
+    public func isEqualToAddress(object: Address?) -> Bool {
+        if let address = object {
+            
+            let haveEqualLatitude = self.latitude == address.latitude
+            let haveEqualLongitude = self.longitude == address.longitude
+            let haveEqualName = self.name == address.name
+            let haveEqualAddressString = self.addressString == address.addressString
+            
+            return haveEqualLatitude && haveEqualLongitude && haveEqualName && haveEqualAddressString
+        }
+        return false
+    }
+    
+    public override func isEqual(object: AnyObject?) -> Bool {
+        if let address = object as? Address {
+            return self.isEqualToAddress(address)
+        }
+        return false
+    }
+    
+}
---
+++ FrostKit/Maps/Annotation.swift
@@ -0,0 +1,74 @@
+//
+//  Annotation.swift
+//  FrostKit
+//
+//  Created by James Barrow on 13/04/2016.
+//  Copyright © 2016 James Barrow - Frostlight Solutions. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+///
+/// An annotation object that is to be used with an address object.
+///
+public class Annotation: NSObject, MKAnnotation {
+    
+    /// The address object for the annotation.
+    public lazy var address = Address()
+    /// The coordinate of the address.
+    public var coordinate: CLLocationCoordinate2D {
+        return address.coordinate
+    }
+    /// The name of the address.
+    public var title: String? {
+        return address.name
+    }
+    /// The address string of the address.
+    public var subtitle: String? {
+        return address.addressString
+    }
+    // If the annotation is a clustered annotation, this value holds all the annotations it represents.
+    public var containdedAnnotations: [Annotation]? {
+        didSet {
+            if containdedAnnotations != nil {
+                clusterAnnotation = nil
+            }
+        }
+    }
+    // If the annotation is part of a clustered annotation, this represent the visable annotation.
+    public var clusterAnnotation: Annotation? {
+        didSet {
+            if clusterAnnotation != nil {
+                containdedAnnotations = nil
+            }
+        }
+    }
+    
+    public override init() {
+        super.init()
+    }
+    
+    /**
+     A convenience method for making an annotation object from an address object.
+     
+     - parameter address: The address object to base the annotation off of.
+     */
+    public convenience init(address: Address) {
+        self.init()
+        
+        self.address = address
+    }
+    
+    /**
+     Updates the annotation with an address.
+     
+     - parameter address: The address to update the annotation with.
+     */
+    public func updateAddress(address: Address) {
+        if self.address.isEqualToAddress(address) == false {
+            self.address = address
+        }
+    }
+    
+}
