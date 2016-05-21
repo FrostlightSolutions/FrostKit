4ca7f5bfb1a6e46e668d27efb0ae1f9b6b7b5673
James Barrow <baza207@me.com>
2016-05-22 00:24:43 +0200

    Updated date extensions for Swift 3.0 and updated naming scheme.

--- FrostKit/Extensions/DateExtensions.swift
+++ FrostKit/Extensions/DateExtensions.swift
@@ -11,19 +11,19 @@ import Foundation
 ///  Different ways to compare date
 public enum DateCompareType {
     /// Date A is before Date B
-    case Before
+    case before
     /// Date A is after Date B
-    case After
+    case after
     /// Both dates are equal
-    case EqualTo
+    case equalTo
     /// Date A is before or equal to Date B
-    case BeforeOrEqualTo
+    case beforeOrEqualTo
     /// Date A is after or equal to Date B
-    case AfterOrEqualTo
+    case afterOrEqualTo
 }
 
 /// Most common component flags
-private let componentFlags: NSCalendarUnit = ([.Year, .Month, .Day, .WeekOfMonth, .Hour, .Minute, .Second, .Weekday, .WeekdayOrdinal])
+private let componentFlags: NSCalendarUnit = ([.year, .month, .day, .weekOfMonth, .hour, .minute, .second, .weekday, .weekdayOrdinal])
 
 ///
 /// Extention functions for NSDate
@@ -39,7 +39,7 @@ extension NSDate {
     
     - returns: The NSDate created from the passed in string or `nil` if it could not be created.
     */
-    public class func fusDate(fusDateString: String) -> NSDate? {
+    public class func fusDate(from fusDateString: String) -> NSDate? {
         var format = "yyyy'-'MM'-'dd"
         if fusDateString.characters.count > 10 {
             format += "'T'HH':'mm':'ss'.'SSSSSS'Z'"
@@ -49,7 +49,7 @@ extension NSDate {
         dateFormatter.dateFormat = format
         dateFormatter.timeZone = NSTimeZone.utc()
         dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-        return dateFormatter.dateFromString(fusDateString)
+        return dateFormatter.date(from: fusDateString)
     }
     
     /**
@@ -59,40 +59,40 @@ extension NSDate {
      
      - returns: The NSDate created from the passed in string or `nil` if it could not be created.
      */
-    public class func iso8601Date(iso8601String: String) -> NSDate? {
+    public class func iso8601Date(from iso8601String: String) -> NSDate? {
         
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
         dateFormatter.timeZone = NSTimeZone.utc()
-        dateFormatter.locale = NSLocale.autoupdatingCurrentLocale()
-        return dateFormatter.dateFromString(iso8601String)
+        dateFormatter.locale = NSLocale.autoupdatingCurrent()
+        return dateFormatter.date(from: iso8601String)
     }
     
     // MARK: - Date Checks
     
     /// `true` if the date is yesterday, `false` if it isn't.
     public var isYesterday: Bool {
-        let date = NSDate().dateByAddingDays(-1)
-        return compareToDate(date, option: DateCompareType.EqualTo)
+        let date = NSDate().dateByAdding(days: -1)
+        return compare(date: date, option: .equalTo)
     }
     
     /// `true` if the date is today, `false` if it isn't.
     public var isToday: Bool {
-        return compareToDate(NSDate(), option: DateCompareType.EqualTo)
+        return compare(date: NSDate(), option: .equalTo)
     }
     
     /// `true` if the date is tomorrow, `false` if it isn't.
     public var isTomorrow: Bool {
-        let date = NSDate().dateByAddingDays(1)
-        return compareToDate(date, option: DateCompareType.EqualTo)
+        let date = NSDate().dateByAdding(days: 1)
+        return compare(date: date, option: .equalTo)
     }
     
     /// `true` if the date is a weekday, `false` if it isn't.
     public var isWeekday: Bool {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Weekday, fromDate: self)
-        let range = calendar.maximumRangeOfUnit(.Weekday)
+        let components = calendar.components(.weekday, from: self)
+        let range = calendar.maximumRange(of: .weekday)
         
         if components.weekday == range.location || components.weekday == range.length {
             return false
@@ -104,7 +104,7 @@ extension NSDate {
     public var isBeginingOfWeek: Bool {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Weekday, fromDate: self)
+        let components = calendar.components(.weekday, from: self)
         return components.weekday == 2 // Begining of week is Monday == 2
     }
     
@@ -112,7 +112,7 @@ extension NSDate {
     public var isEndOfWeek: Bool {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Weekday, fromDate: self)
+        let components = calendar.components(.weekday, from: self)
         return components.weekday == 1 // End of week is Sunday == 1
     }
     
@@ -120,7 +120,7 @@ extension NSDate {
     public var isBeginingOfMonth: Bool {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Day, fromDate: self)
+        let components = calendar.components(.day, from: self)
         return components.day == 1
     }
     
@@ -128,8 +128,8 @@ extension NSDate {
     public var isEndOfMonth: Bool {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Day, fromDate: self)
-        let range = calendar.rangeOfUnit(.Day, inUnit: .Month, forDate: self)
+        let components = calendar.components(.day, from: self)
+        let range = calendar.range(of: .day, in: .month, for: self)
         return components.day == range.length
     }
     
@@ -139,7 +139,7 @@ extension NSDate {
     public var day: Int {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Day, fromDate: self)
+        let components = calendar.components(.day, from: self)
         return components.day
     }
     
@@ -147,7 +147,7 @@ extension NSDate {
     public var hour: Int {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Hour, fromDate: self)
+        let components = calendar.components(.hour, from: self)
         return components.hour
     }
     
@@ -155,7 +155,7 @@ extension NSDate {
     public var minute: Int {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Minute, fromDate: self)
+        let components = calendar.components(.minute, from: self)
         return components.minute
     }
     
@@ -198,10 +198,10 @@ extension NSDate {
         var dateTo: NSDate?
         
         let calendar = NSCalendar.iso8601Calendar()
-        calendar.rangeOfUnit(.Day, startDate: &dateTo, interval: nil, forDate: toDate)
-        calendar.rangeOfUnit(.Day, startDate: &dateFrom, interval: nil, forDate: fromDate)
+        calendar.range(of: .day, start: &dateTo, interval: nil, for: toDate)
+        calendar.range(of: .day, start: &dateFrom, interval: nil, for: fromDate)
         
-        let components = calendar.components(.Day, fromDate: dateFrom!, toDate: dateTo!, options: .WrapComponents)
+        let components = calendar.components(.day, from: dateFrom!, to: dateTo!, options: .wrapComponents)
         return components.day
     }
     
@@ -209,7 +209,7 @@ extension NSDate {
     public var daysRemainingInWeek: Int {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Weekday, fromDate: self)
+        let components = calendar.components(.weekday, from: self)
         var weekdayOfDate = components.weekday
         // To make Monday == 1
         weekdayOfDate -= 1
@@ -223,7 +223,7 @@ extension NSDate {
     public var daysInMonth: Int {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let range = calendar.rangeOfUnit(.Day, inUnit: .Month, forDate: self)
+        let range = calendar.range(of: .day, in: .month, for: self)
         return range.length
     }
     
@@ -231,7 +231,7 @@ extension NSDate {
     public var daysRemainingInMonth: Int {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(.Day, fromDate: self)
+        let components = calendar.components(.day, from: self)
         return (daysInMonth - components.day)
     }
     
@@ -248,28 +248,28 @@ extension NSDate {
     
     - returns: `true` if the dates conform with the date comparison type check, `false` if not
     */
-    public func compareToDate(date: NSDate, option: DateCompareType, stripTime: Bool = true) -> Bool {
+    public func compare(date: NSDate, option: DateCompareType, stripTime: Bool = true) -> Bool {
         
-        var compare: NSComparisonResult = .OrderedSame
+        var compare: NSComparisonResult = .orderedSame
         if stripTime == true {
             compare = self.stripTime.compare(date.stripTime)
         } else {
             compare = self.compare(date)
         }
         
-        if compare == NSComparisonResult.OrderedAscending {
+        if compare == NSComparisonResult.orderedAscending {
             
-            if option == .Before || option == .BeforeOrEqualTo {
+            if option == .before || option == .beforeOrEqualTo {
                 return true
             }
-        } else if compare == NSComparisonResult.OrderedDescending {
+        } else if compare == NSComparisonResult.orderedDescending {
             
-            if option == .After || option == .AfterOrEqualTo {
+            if option == .after || option == .afterOrEqualTo {
                 return true
             }
         } else {
             
-            if option == .EqualTo || option == .BeforeOrEqualTo || option == .AfterOrEqualTo {
+            if option == .equalTo || option == .beforeOrEqualTo || option == .afterOrEqualTo {
                 return true
             }
         }
@@ -285,7 +285,7 @@ extension NSDate {
     - returns: `true` if the comparison date is before date, `false` if not
     */
     public func isBefore(date: NSDate) -> Bool {
-        return compareToDate(date, option: .Before)
+        return compare(date: date, option: .before)
     }
     
     /**
@@ -296,7 +296,7 @@ extension NSDate {
     - returns: `true` if the comparison date is after date, `false` if not
     */
     public func isAfter(date: NSDate) -> Bool {
-        return compareToDate(date, option: .After)
+        return compare(date: date, option: .after)
     }
     
     // MARK: -
@@ -305,9 +305,9 @@ extension NSDate {
     public var stripTime: NSDate {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(([.Day, .Month, .Year]), fromDate: self)
+        let components = calendar.components(([.day, .month, .year]), from: self)
         components.timeZone = NSTimeZone.utc()
-        return calendar.dateFromComponents(components)!
+        return calendar.date(from: components)!
     }
     
     /**
@@ -317,7 +317,7 @@ extension NSDate {
     
     - returns: A copy of the current date `days` added to it
     */
-    public func dateByAddingDays(days: Int) -> NSDate {
+    public func dateByAdding(days: Int) -> NSDate {
         
         if days == 0 {
             return self
@@ -327,69 +327,69 @@ extension NSDate {
         components.day = days
         
         let calendar = NSCalendar.iso8601Calendar()
-        return calendar.dateByAddingComponents(components, toDate: self, options: .SearchBackwards)!
+        return calendar.date(byAdding: components, to: self, options: .searchBackwards)!
     }
     
     /// Returns a date with the time at the start of the day, while preserving the time zone.
     public var dateAtStartOfDay: NSDate {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(componentFlags, fromDate: self)
+        let components = calendar.components(componentFlags, from: self)
         components.timeZone = NSTimeZone.utc()
         components.hour = 0
         components.minute = 0
         components.second = 0
-        return calendar.dateFromComponents(components)!
+        return calendar.date(from: components)!
     }
     
     /// Returns a date with the time at the end of the day, while preserving the time zone.
     public var dateAtEndOfDay: NSDate {
         
         let calendar = NSCalendar.iso8601Calendar()
-        let components = calendar.components(componentFlags, fromDate: self)
+        let components = calendar.components(componentFlags, from: self)
         components.timeZone = NSTimeZone.utc()
         components.hour = 23
         components.minute = 59
         components.second = 59
-        return calendar.dateFromComponents(components)!
+        return calendar.date(from: components)!
     }
     
     // MARK: - Date Strings
     
     /// A helper method for getting a formatted string of the date and time in `ShortStyle`
     public var dateTimeShortString: String {
-        return  NSDateFormatter.localizedStringFromDate(self, dateStyle: .ShortStyle, timeStyle: .ShortStyle)
+        return  NSDateFormatter.localizedString(from: self, dateStyle: .shortStyle, timeStyle: .shortStyle)
     }
     
     /// A helper method for getting a formatted string of the date in `ShortStyle`
     public var dateShortString: String {
-        return  NSDateFormatter.localizedStringFromDate(self, dateStyle: .ShortStyle, timeStyle: .NoStyle)
+        return  NSDateFormatter.localizedString(from: self, dateStyle: .shortStyle, timeStyle: .noStyle)
     }
     
     /// A helper method for getting a formatted string of the date in `MediumStyle`
     public var dateMediumString: String {
-        return  NSDateFormatter.localizedStringFromDate(self, dateStyle: .MediumStyle, timeStyle: .NoStyle)
+        return  NSDateFormatter.localizedString(from: self, dateStyle: .mediumStyle, timeStyle: .noStyle)
     }
     
     /// A helper method for getting a formatted string of the date in `FullStyle`
     public var dateFullString: String {
-        return  NSDateFormatter.localizedStringFromDate(self, dateStyle: .FullStyle, timeStyle: .NoStyle)
+        return  NSDateFormatter.localizedString(from: self, dateStyle: .fullStyle, timeStyle: .noStyle)
     }
     
     /// A helper method for getting a formatted string of the time in `ShortStyle`
     public var timeShortString: String {
-        return  NSDateFormatter.localizedStringFromDate(self, dateStyle: .NoStyle, timeStyle: .ShortStyle)
+        return  NSDateFormatter.localizedString(from: self, dateStyle: .noStyle, timeStyle: .shortStyle)
     }
     
     /// A helpter method for getting a formatted string of the date in the FUS set format.
     public var fusDateString: String {
-        return dateStringFromFormat("yyyy'-'MM'-'dd")
+        return dateString(fromFormat: "yyyy'-'MM'-'dd")
     }
     
     /// A helpter method for getting a formatted string of the date and time in the FUS set format.
     public var fusDateTimeString: String {
         let locale = NSLocale(localeIdentifier: "en_US_POSIX")
-        return dateStringFromFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSSSSS'Z'", locale: locale)
+        return dateString(fromFormat: "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSSSSS'Z'", locale: locale)
     }
     
     /**
@@ -400,36 +400,36 @@ extension NSDate {
     
     - returns: The formatted date string.
     */
-    public func dateStringFromFormat(format: String, locale: NSLocale? = nil) -> String {
+    public func dateString(fromFormat format: String, locale: NSLocale? = nil) -> String {
         let formatter = NSDateFormatter()
         formatter.dateFormat = format
         formatter.locale = locale
-        return formatter.stringFromDate(self)
+        return formatter.string(from: self)
     }
     
     /// Returns the date's day of the week string. i.e. Monday
     public var dayString: String {
-        return dateStringFromFormat("EEEE")
+        return dateString(fromFormat: "EEEE")
     }
     
     /// Returns the date's day of the week as a short string. i.e. Mon
     public var dayShortString: String {
-        return dateStringFromFormat("EEE")
+        return dateString(fromFormat: "EEE")
     }
     
     /// Returns the date's month string. i.e. September
     public var monthString: String {
-        return dateStringFromFormat("MMMM")
+        return dateString(fromFormat: "MMMM")
     }
     
     /// Returns the date's month short string. i.e. Sept
     public var monthShortString: String {
-        return dateStringFromFormat("MMM")
+        return dateString(fromFormat: "MMM")
     }
     
     /// Returns the date's year string. i.e. 2015
     public var yearString: String {
-        return dateStringFromFormat("Y")
+        return dateString(fromFormat: "Y")
     }
     
 }
