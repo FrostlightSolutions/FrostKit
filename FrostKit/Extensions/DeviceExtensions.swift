5d6b8cf37809b1a7e9af2d0a8c9d69b0ca12d9e5
James Barrow <baza207@me.com>
2016-05-22 00:35:56 +0200

    Updated device extensions for Swift 3.0 and updated naming scheme.

--- FrostKit/Extensions/DeviceExtensions.swift
+++ FrostKit/Extensions/DeviceExtensions.swift
@@ -18,29 +18,29 @@ extension UIDevice {
     */
     struct SystemVersion {
         /// The full system version.
-        static let version = UIDevice.currentDevice().systemVersion
-        private static let versionComponents = version.componentsSeparatedByString(".")
+        static let version = UIDevice.current().systemVersion
+        private static let versionComponents = version.components(separatedBy: ".")
         /// The major system version.
         static var majorVersion: Int {
-            return UIDevice.systemVersionAtIndex(.Major, components: versionComponents)
+            return UIDevice.systemVersion(atIndex: .major, components: versionComponents)
         }
         /// The minor system version.
         static var minorVersion: Int {
-            return UIDevice.systemVersionAtIndex(.Minor, components: versionComponents)
+            return UIDevice.systemVersion(atIndex: .minor, components: versionComponents)
         }
         /// The minor path version.
         static var bugFixVersion: Int {
-            return UIDevice.systemVersionAtIndex(.BugFix, components: versionComponents)
+            return UIDevice.systemVersion(atIndex: .bugFix, components: versionComponents)
         }
     }
     
     private enum VersionIndex: Int {
-        case Major = 0
-        case Minor
-        case BugFix
+        case major = 0
+        case minor
+        case bugFix
     }
     
-    private class func systemVersionAtIndex(index: VersionIndex, components: [String]) -> Int {
+    private class func systemVersion(atIndex index: VersionIndex, components: [String]) -> Int {
         if let version = Int(components[index.rawValue]) where components.count > index.rawValue {
             return version
         }
