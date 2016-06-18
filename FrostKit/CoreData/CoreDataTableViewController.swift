10a599e693e6712130779c12661ed35b0dfbb508
James Barrow <baza207@me.com>
2016-06-18 23:22:05 +0200

    Split core data controller methods out into there own class, and simplified core data table view controller.

---
+++ FrostKit/CoreData/CoreDataTableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  CoreDataTableViewController.swift
+//  FrostKit
+//
+//  Created by James Barrow on 18/06/2016.
+//  Copyright (c) 2015-Current Pig on a Hill Productions. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+public class CoreDataTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    // MARK: - Properties
+    
+    @IBOutlet public weak var dataController: CoreDataController! {
+        didSet { dataController.fetchedResultsController.delegate = self }
+    }
+    public var fetchedResultsController: NSFetchedResultsController {
+        return dataController.fetchedResultsController
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        clearsSelectionOnViewWillAppear = true
+    }
+    
+    override public func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Refresh data
+        do {
+            try fetchedResultsController.performFetch()
+        } catch let error as NSError {
+            NSLog("Fetch error: \(error.localizedDescription)\n\(error)")
+        }
+    }
+    
+    // MARK: - Table view
+    
+    override public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    override public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let sections = fetchedResultsController.sections where sections.count > section {
+            
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+            
+        } else {
+            return 0
+        }
+    }
+    
+    // MARK: - Fetched results controller
+    
+    public func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.reloadData()
+    }
+
+}
