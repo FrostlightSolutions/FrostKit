a564512f39bbcf0e6af2e67d6c62d79bb7086fe6
James Barrow <baza207@me.com>
2019-06-28 20:48:18 +0200

    Depreciated network request notification names

--- FrostKit/FrostKit.swift
+++ FrostKit/FrostKit.swift
@@ -31,7 +31,10 @@ public typealias Font = UIFont
 // swiftlint:disable variable_name
 public let FUSServiceClientUpdateSections = "com.FrostKit.FUSServiceClient.updateSections"
 public let UserStoreLogoutClearData = "com.FrostKit.UserStore.logout.clearData"
+
+@available(iOS, deprecated: 13.0, message: "This is no longer needed as of iOS 13 as the network indicator has been deprecated. This class will be removed in v2.0.0 of FrostKit.")
 public let NetworkRequestDidBeginNotification = "com.FrostKit.activityIndicator.request.begin"
+@available(iOS, deprecated: 13.0, message: "This is no longer needed as of iOS 13 as the network indicator has been deprecated. This class will be removed in v2.0.0 of FrostKit.")
 public let NetworkRequestDidCompleteNotification = "com.FrostKit.activityIndicator.request.complete"
 // swiftlint:enable variable_name
 
